be6e791cb31e8f52de0c63dec861b3a6f67ca719
WuDongyang <wudongyang@gmail.com>
2018-10-30 16:41:49 +0800

    根据最新版本调整 mk文件，但编译依然有问题

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -8,52 +8,48 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := xmrig
 XMRIG_SRC_PATH := ../../
 
+
 LOCAL_SRC_FILES :=    \
-    $(XMRIG_SRC_PATH)/src/api/Api.cpp \
-    $(XMRIG_SRC_PATH)/src/api/ApiState.cpp \
     $(XMRIG_SRC_PATH)/src/api/NetworkState.cpp \
     $(XMRIG_SRC_PATH)/src/App.cpp \
-    $(XMRIG_SRC_PATH)/src/Console.cpp \
-    $(XMRIG_SRC_PATH)/src/log/ConsoleLog.cpp \
-    $(XMRIG_SRC_PATH)/src/log/FileLog.cpp \
-    $(XMRIG_SRC_PATH)/src/log/Log.cpp \
+    $(XMRIG_SRC_PATH)/src/common/config/CommonConfig.cpp \
+    $(XMRIG_SRC_PATH)/src/common/config/ConfigLoader.cpp \
+    $(XMRIG_SRC_PATH)/src/common/config/ConfigWatcher.cpp \
+    $(XMRIG_SRC_PATH)/src/common/Console.cpp \
+    $(XMRIG_SRC_PATH)/src/common/crypto/Algorithm.cpp \
+    $(XMRIG_SRC_PATH)/src/common/crypto/keccak.cpp \
+    $(XMRIG_SRC_PATH)/src/common/log/BasicLog.cpp \
+    $(XMRIG_SRC_PATH)/src/common/log/ConsoleLog.cpp \
+    $(XMRIG_SRC_PATH)/src/common/log/FileLog.cpp \
+    $(XMRIG_SRC_PATH)/src/common/log/Log.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/Client.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/Job.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/Pool.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/strategies/FailoverStrategy.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/strategies/SinglePoolStrategy.cpp \
+    $(XMRIG_SRC_PATH)/src/common/net/SubmitResult.cpp \
+    $(XMRIG_SRC_PATH)/src/common/Platform.cpp \
+    $(XMRIG_SRC_PATH)/src/core/Config.cpp \
+    $(XMRIG_SRC_PATH)/src/core/Controller.cpp \
     $(XMRIG_SRC_PATH)/src/Mem.cpp \
-    $(XMRIG_SRC_PATH)/src/net/Client.cpp \
-    $(XMRIG_SRC_PATH)/src/net/Job.cpp \
     $(XMRIG_SRC_PATH)/src/net/Network.cpp \
     $(XMRIG_SRC_PATH)/src/net/strategies/DonateStrategy.cpp \
-    $(XMRIG_SRC_PATH)/src/net/strategies/FailoverStrategy.cpp \
-    $(XMRIG_SRC_PATH)/src/net/strategies/SinglePoolStrategy.cpp \
-    $(XMRIG_SRC_PATH)/src/net/SubmitResult.cpp \
-    $(XMRIG_SRC_PATH)/src/net/Url.cpp \
-    $(XMRIG_SRC_PATH)/src/Options.cpp \
-    $(XMRIG_SRC_PATH)/src/Platform.cpp \
     $(XMRIG_SRC_PATH)/src/Summary.cpp \
-    $(XMRIG_SRC_PATH)/src/workers/DoubleWorker.cpp \
+    $(XMRIG_SRC_PATH)/src/workers/CpuThread.cpp \
     $(XMRIG_SRC_PATH)/src/workers/Handle.cpp \
     $(XMRIG_SRC_PATH)/src/workers/Hashrate.cpp \
-    $(XMRIG_SRC_PATH)/src/workers/SingleWorker.cpp \
+    $(XMRIG_SRC_PATH)/src/workers/MultiWorker.cpp \
     $(XMRIG_SRC_PATH)/src/workers/Worker.cpp \
     $(XMRIG_SRC_PATH)/src/workers/Workers.cpp \
     $(XMRIG_SRC_PATH)/src/xmrig.cpp \
-    $(XMRIG_SRC_PATH)/src/App_unix.cpp \
-    $(XMRIG_SRC_PATH)/src/Cpu_unix.cpp \
-    $(XMRIG_SRC_PATH)/src/Mem_unix.cpp \
-    $(XMRIG_SRC_PATH)/src/crypto/c_keccak.c \
-    $(XMRIG_SRC_PATH)/src/crypto/c_groestl.c \
-    $(XMRIG_SRC_PATH)/src/crypto/c_blake256.c \
-    $(XMRIG_SRC_PATH)/src/crypto/c_jh.c \
-    $(XMRIG_SRC_PATH)/src/crypto/c_skein.c \
-    $(XMRIG_SRC_PATH)/src/crypto/CryptoNight.cpp \
-    $(XMRIG_SRC_PATH)/src/Platform_unix.cpp \
     $(XMRIG_SRC_PATH)/src/Rand_android.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src $(LOCAL_PATH)/../libuv/include $(LOCAL_PATH)/../../src/3rdparty
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src $(LOCAL_PATH)/../../src/common $(LOCAL_PATH)/../libuv/include $(LOCAL_PATH)/../../src/3rdparty
 
 LOCAL_CPPFLAGS += -pthread  -frtti -std=c++11  -Wall -fno-exceptions -fno-rtti -Wno-missing-braces
 LOCAL_CFLAGS   += -Ofast -funroll-loops -fmerge-all-constants -flax-vector-conversions
 LOCAL_CPPFLAGS += -Ofast -funroll-loops -fmerge-all-constants -flax-vector-conversions
-LOCAL_CFLAGS +=   -DXMRIG_NO_HTTPD -DXMRIG_NO_API -DXMRIG_NO_LIBCPUID
+LOCAL_CFLAGS +=   -DXMRIG_NO_HTTPD -DXMRIG_NO_API -DXMRIG_NO_LIBCPUID -DXMRIG_NO_TLS -DXMRIG_NO_ASM -DXMRIG_ARM
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 LOCAL_CFLAGS   += -mfpu=neon -march=armv7-a
@@ -66,12 +62,11 @@ endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 LOCAL_CFLAGS += -DXMRIG_ARMv7 -DXMRIG_ARM
 LOCAL_LDLIBS +=  -luv_armv7
-LOCAL_SRC_FILES += $(XMRIG_SRC_PATH)/src/Cpu_arm.cpp
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 LOCAL_CFLAGS += -DXMRIG_ARMv8 -DXMRIG_ARMv7 -DXMRIG_ARM
 LOCAL_LDLIBS +=  -luv_armv8
-LOCAL_SRC_FILES += $(XMRIG_SRC_PATH)/src/Cpu_arm.cpp
 endif
+
 
 LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../libuv/lib
 
